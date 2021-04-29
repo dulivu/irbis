21210d5938b739041d8b2756f565e8dd31eff544
Jorge Quico C <GeorgeL1102@gmail.com>
2021-04-29 15:21:44 -0500

    Update Json.php

--- Json.php
+++ Json.php
@@ -19,7 +19,12 @@ class Json {
 		JSON_ERROR_STATE_MISMATCH => 'JSON, Invalid or malformed JSON',
 		JSON_ERROR_CTRL_CHAR => 'JSON, Control character error, possibly incorrectly encoded',
 		JSON_ERROR_SYNTAX => 'JSON, Syntax error',
-		JSON_ERROR_UTF8 => 'JSON, Malformed UTF-8 characters, possibly incorrectly encoded'
+		JSON_ERROR_UTF8 => 'JSON, Malformed UTF-8 characters, possibly incorrectly encoded',
+		JSON_ERROR_RECURSION => 'JSON, One or more recursive references',
+		JSON_ERROR_INF_OR_NAN => 'JSON, One or more NAN or INF values',
+		JSON_ERROR_UNSUPPORTED_TYPE => 'JSON, A value of a type that cannot be encoded was provided',
+		JSON_ERROR_INVALID_PROPERTY_NAME => 'JSON, A property name was given that cannot be encoded',
+		JSON_ERROR_UTF16 => 'JSON, Malformed UTF-16 characters, possibly incorrectly encoded',
 	);
 
 	public static function encode($value, $options = JSON_UNESCAPED_UNICODE) {
